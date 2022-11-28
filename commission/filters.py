fc51ac4fb7e13eddafe305acdcb23aeb02bbac8a
Matheus Diori <mdiori@hotmail.com>
2022-11-28 10:05:33 -0300

    fix: fix commission filter class name

--- commission/filters.py
+++ commission/filters.py
@@ -2,7 +2,7 @@ from typing import List
 from core.filters import BaseFilter, FilterParam
 
 
-class ClientFilter(BaseFilter):
+class CommissionFilter(BaseFilter):
     query_params: List[FilterParam] = [
         FilterParam('week_day', {'type': 'integer'}, 'week_day__icontains'),
         FilterParam('active', {'type': 'boolean'}, 'active')
