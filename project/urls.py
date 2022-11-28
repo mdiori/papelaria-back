0ff355bf82480c2f3518d0ee15249aafa6d96eeb
Matheus Diori <mdiori@hotmail.com>
2022-11-27 23:18:14 -0300

    feat: add project routes

--- project/urls.py
+++ project/urls.py
@@ -6,4 +6,8 @@ API_PFX = 'api/v1.0'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f'{API_PFX}/product/', include('product.urls')),
+    path(f'{API_PFX}/client/', include('client.urls')),
+    path(f'{API_PFX}/employee/', include('employee.urls')),
+    path(f'{API_PFX}/sale/', include('sale.urls')),
+    path(f'{API_PFX}/commission/', include('commission.urls')),
 ]
