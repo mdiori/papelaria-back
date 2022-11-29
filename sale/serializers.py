d4996e199f4d3f741b8b8cc1daa2527924a8c25d
Matheus Diori <mdiori@hotmail.com>
2022-11-28 23:18:34 -0300

    fix: add missing fields to serializers
      * add sale and product field to serializer
      * add client and employee to serializer and its representations

--- sale/serializers.py
+++ sale/serializers.py
@@ -2,13 +2,16 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from sale.models import Sale, SaleProduct
 from product.serializers import ProductSerializer
+from client.serializers import ClientSerializer
+from employee.serializers import EmployeeSerializer
 from rest_framework.validators import UniqueValidator
 
 
 class SaleProductModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = SaleProduct
-        fields = ['id', 'quantity', 'created_at', 'updated_at', ]
+        fields = ['id', 'quantity', 'sale',
+                  'product', 'created_at', 'updated_at', ]
         read_only_fields = ['id', 'created_at', 'updated_at', ]
 
     def to_representation(self, instance):
@@ -29,7 +32,7 @@ class SaleProductModelSerializer(serializers.ModelSerializer):
 class SaleModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
-        fields = ['id', 'date', 'commission_min',
+        fields = ['id', 'date', 'commission_min', 'client', 'employee',
                   'commission_max', 'invoice', ]
         read_only_fields = ['id', 'created_at', 'updated_at']
         extra_kwargs = {
@@ -42,6 +45,14 @@ class SaleModelSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
+
+        rep['client'] = ClientSerializer(
+            instance.client
+        ).data
+
+        rep['employee'] = EmployeeSerializer(
+            instance.employee
+        ).data
 
         rep['sale_products'] = SaleProductModelSerializer(
             instance.saleproduct_set.all(),
