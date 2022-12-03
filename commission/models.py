0346e731e525a79b6f41419f20ec86a379a8a4e1
Matheus Diori <mdiori@hotmail.com>
2022-12-03 18:14:05 -0300

    refactor: commission model
         * create week choices
         * add unique week field

--- commission/models.py
+++ commission/models.py
@@ -4,8 +4,27 @@ from uuid import uuid4
 
 
 class Commission(SoftDeleteModel):
+
+    class WEEK_DAY(models.IntegerChoices):
+        MONDAY = 0
+        TUESDAY = 1
+        WEDNESDAY = 2
+        THURSDAY = 3
+        FRIDAY = 4
+        SATURDAY = 5
+        SUNDAY = 6
+
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    week_day = models.PositiveIntegerField()
-    commission_min = models.PositiveIntegerField()
-    commission_max = models.PositiveIntegerField()
-    active = models.BooleanField(default=True)
+    week_day = models.PositiveIntegerField(choices=WEEK_DAY.choices)
+    commission_min = models.PositiveIntegerField(default=0)
+    commission_max = models.PositiveIntegerField(default=1000)
+
+    class Meta:
+        ordering = ['week_day', ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=['week_day', ],
+                condition=models.Q(deleted_at=None),
+                name='unique_week_day_if_not_deleted'
+            ),
+        ]
