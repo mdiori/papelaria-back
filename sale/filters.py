736f3541e1a74e7daa32f66d6f106afaa137079d
Matheus Diori <mdiori@hotmail.com>
2022-12-05 06:01:03 -0300

    feat: add commissions date filters

--- sale/filters.py
+++ sale/filters.py
@@ -5,4 +5,6 @@ from core.filters import BaseFilter, FilterParam
 class SaleFilter(BaseFilter):
     query_params: List[FilterParam] = [
         FilterParam('invoice', {'type': 'string'}, 'invoice__icontains'),
+        FilterParam('initial_date', {'type': 'string'}, 'date__gte'),
+        FilterParam('end_date', {'type': 'string'}, 'date__lte'),
     ]
