01acf29172540ec226acdd66ec3e6710c7298bcb
Matheus <matheus.diori@amcom.com.br>
2024-10-28 23:40:58 -0300

    chore: Add volume for fast reload on development

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,33 +1,28 @@
 #!/bin/sh
 
+# Concede permissão de execução para o próprio script
+chmod +x /app/scripts/entrypoint.sh
+
 echo "Waiting for PostgreSQL to start..."
 while ! nc -z db 5432; do
   sleep 0.1
 done
 echo "PostgreSQL started"
 
-# Check if this is the first-time run
-if [ ! -f "/app/.initialized" ]; then
-    echo "-----> Making migrations"
-    python manage.py makemigrations
+echo "-----> Making migrations"
+python manage.py makemigrations
 
-    echo "-----> Migrating"
-    python manage.py migrate
+echo "-----> Migrating"
+python manage.py migrate
 
-    echo "-----> Creating superuser"
-    python manage.py shell < scripts/create_user.py
+echo "-----> Creating superuser"
+python manage.py shell < scripts/create_user.py
 
-    echo "-----> Seeding database"
-    python manage.py seedCommissions
-    python manage.py seedClients
-    python manage.py seedEmployees
-    python manage.py seedProducts
-
-    # Create a marker file to indicate initialization has been completed
-    touch /app/.initialized
-else
-    echo "Initialization already completed. Skipping..."
-fi
+echo "-----> Seeding database"
+python manage.py seedCommissions
+python manage.py seedClients
+python manage.py seedEmployees
+python manage.py seedProducts
 
 echo "-----> Starting server"
-exec python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
