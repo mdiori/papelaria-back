6add544d09cc3d6562d19ceedfbef397d817f625
Matheus <matheus.diori@amcom.com.br>
2024-10-29 09:58:27 -0300

    feat: Change week day visualization and system language

--- commission/admin.py
+++ commission/admin.py
@@ -6,9 +6,20 @@ class CommissionAdmin(admin.ModelAdmin):
     list_filter = ('week_day',)
     search_fields = ('commission_min', 'commission_max')
 
-    def get_week_day_display(self, obj):
-        """Exibe o dia da semana no formato legível."""
+    """ def get_week_day_display(self, obj):
         return obj.get_week_day_display()
-    get_week_day_display.short_description = 'Dia da Semana'
+    get_week_day_display.short_description = 'Dia da Semana' """
+    
+    def get_week_day_display(self, obj):
+        dias_semana = {
+            0: "Segunda-feira",
+            1: "Terça-feira",
+            2: "Quarta-feira",
+            3: "Quinta-feira",
+            4: "Sexta-feira",
+            5: "Sábado",
+            6: "Domingo"
+        }
+        return dias_semana.get(obj.week_day, "Desconhecido")
 
 admin.site.register(Commission, CommissionAdmin)
