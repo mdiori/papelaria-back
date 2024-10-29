fa621106245efeb232616b216c7fb07a13b51a4f
Matheus <matheus.diori@amcom.com.br>
2024-10-28 23:16:58 -0300

    chore: Add verification to run script only on build

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,20 +6,28 @@ while ! nc -z db 5432; do
 done
 echo "PostgreSQL started"
 
-echo "-----> Making migrations"
-python manage.py makemigrations
+# Check if this is the first-time run
+if [ ! -f "/app/.initialized" ]; then
+    echo "-----> Making migrations"
+    python manage.py makemigrations
 
-echo "-----> Migrating"
-python manage.py migrate
+    echo "-----> Migrating"
+    python manage.py migrate
 
-echo "-----> Creating superuser"
-python manage.py shell < scripts/create_user.py
+    echo "-----> Creating superuser"
+    python manage.py shell < scripts/create_user.py
 
-echo "-----> Seeding database"
-python manage.py seedCommissions
-python manage.py seedClients
-python manage.py seedEmployees
-python manage.py seedProducts
+    echo "-----> Seeding database"
+    python manage.py seedCommissions
+    python manage.py seedClients
+    python manage.py seedEmployees
+    python manage.py seedProducts
+
+    # Create a marker file to indicate initialization has been completed
+    touch /app/.initialized
+else
+    echo "Initialization already completed. Skipping..."
+fi
 
 echo "-----> Starting server"
-python manage.py runserver 0.0.0.0:8000
+exec python manage.py runserver 0.0.0.0:8000
